2bf200e8dffb1732ec3d60d7b25f62bc64e73901
Akshay Iyer <akshay.iyer@nyu.edu>
2016-06-19 19:29:25 -0400

    Made a post call for login section

--- iOS Test/IOSProgrammerTest/API Section/LoginSectionViewController.m
+++ iOS Test/IOSProgrammerTest/API Section/LoginSectionViewController.m
@@ -19,8 +19,7 @@
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+  
 }
 
 - (void)didReceiveMemoryWarning
@@ -28,11 +27,35 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)loginButton:(id)sender {
+    NSMutableURLRequest *req = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://dev.apppartner.com/AppPartnerProgrammerTest/scripts/login.php"]
+                            cachePolicy:NSURLRequestReloadIgnoringCacheData
+                        timeoutInterval:90];
+    NSMutableString *postrequest = [NSMutableString string];
+    [postrequest appendFormat:@"&%@=%@", @"username", @"SuperBoise"];
+    [postrequest appendFormat:@"&%@=%@", @"password", @"qwerty"];
+    
+    NSData *requestData = [postrequest dataUsingEncoding:NSUTF8StringEncoding];
+    [req setHTTPMethod:@"POST"];
+    [req setValue:@"application/x-www-form-urlencoded;charset=UTF-8" forHTTPHeaderField:@"Content-Type"];
+    [req setValue:[NSString stringWithFormat:@"%lu", (unsigned long)requestData.length] forHTTPHeaderField:@"Content-Length"];
+    [req setHTTPBody:requestData];
+    
+    //NSData *outData = [NSURLConnection sendSynchronousRequest:req returningResponse:&response error:&error];
+    [NSURLConnection sendAsynchronousRequest:req queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
+        NSString *strData = [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
+        NSLog(@"%@",strData);
+    }];
+    //NSDictionary *dictionary = [NSJSONSerialization JSONObjectWithData:outData options:0 error:NULL];
+    
+}
 
 - (IBAction)backAction:(id)sender
 {
     MainMenuViewController *mainMenuViewController = [[MainMenuViewController alloc] init];
     [self.navigationController pushViewController:mainMenuViewController animated:YES];
 }
+
+
 
 @end
